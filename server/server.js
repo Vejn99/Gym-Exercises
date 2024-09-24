65611a869025193b1a793584457ddd017c90d15a
Vane Stoev <Vanestoev99@gmail.com>
2024-09-24 14:52:23 +0200

    added backend

--- server/server.js
+++ server/server.js
@@ -1,14 +1,35 @@
+require("dotenv").config();
+
 const express = require("express");
+const mongoose = require("mongoose");
+const workoutRoutes = require("./routes/workouts");
 
 // express app start
 const app = express();
 
-// routes
-app.get("/", (req, res) => {
-  res.json({ mssg: "Welcome to the app" });
+// middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log("new request made:");
+  console.log("host: ", req.hostname);
+  console.log("path: ", req.path);
+  console.log("method: ", req.method);
+  next();
 });
 
-// listen for requests
-app.listen(4000, () => {
-  console.log("listening on port 4000");
-});
+// routes
+app.use("/api/workouts", workoutRoutes);
+
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("connected to db & listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
